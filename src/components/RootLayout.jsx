67ef044331418b445e8ecd34e9b550f8c1970b83
lidevlin <liam.devlin@iqvia.com>
2023-08-17 23:20:24 +0100

    Edited the top navigation colour

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -18,10 +18,10 @@ import { Container } from '@/components/Container'
 import { Footer } from '@/components/Footer'
 import { GridPattern } from '@/components/GridPattern'
 import { Logo, Logomark } from '@/components/Logo'
-import { Mulish } from 'next/font/google';
+import { Mulish } from 'next/font/google'
 
 const RootLayoutContext = createContext({})
-const mulish = Mulish({ subsets: ['latin'] });
+const mulish = Mulish({ subsets: ['latin'] })
 
 function XIcon(props) {
   return (
@@ -61,17 +61,16 @@ function Header({
         >
           <Logomark
             className="h-8 sm:hidden"
-            invert={invert}
             filled={logoHovered}
           />
           <Logo
             className="hidden h-8 sm:block"
-            invert={invert}
+
             filled={logoHovered}
           />
         </Link>
         <div className="flex items-center gap-x-8">
-          <Button href="/contact" invert={invert}>
+          <Button href="/contact">
             Contact Me
           </Button>
           <button
@@ -81,17 +80,15 @@ function Header({
             aria-expanded={expanded.toString()}
             aria-controls={panelId}
             className={clsx(
-              'group -m-2.5 rounded-full p-2.5 transition',
-              invert ? 'hover:bg-white/10' : 'hover:bg-neutral-950/10'
+              'group -m-2.5 rounded-full p-2.5 transition hover:bg-neutral-950/10'
             )}
             aria-label="Toggle navigation"
           >
             <Icon
               className={clsx(
                 'h-6 w-6',
-                invert
-                  ? 'fill-white group-hover:fill-neutral-200'
-                  : 'fill-neutral-950 group-hover:fill-neutral-700'
+
+                'fill-neutral-950 group-hover:fill-neutral-700'
               )}
             />
           </button>
@@ -103,7 +100,7 @@ function Header({
 
 function NavigationRow({ children }) {
   return (
-    <div className="even:mt-px sm:bg-neutral-950">
+    <div className="even:mt-px sm:bg-stone-200">
       <Container>
         <div className="grid grid-cols-1 sm:grid-cols-2">{children}</div>
       </Container>
@@ -115,17 +112,19 @@ function NavigationItem({ href, children }) {
   return (
     <Link
       href={href}
-      className="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
+      className="group relative isolate -mx-6 bg-stone-200  px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-stone-300  sm:even:pl-16"
     >
       {children}
-      <span className="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
+      <span className="absolute inset-y-0 -z-10 w-screen bg-stone-200 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:bg-stone-300 group-hover:opacity-40" />
     </Link>
   )
 }
 
 function Navigation() {
   return (
-    <nav className={`mt-px font-display text-5xl font-medium tracking-tight text-white ${mulish.className}`}>
+    <nav
+      className={`mt-px font-display text-5xl font-medium tracking-tight text-zinc-500 ${mulish.className} border-2 border-stone-300`}
+    >
       <NavigationRow>
         <NavigationItem href="/about">About Me</NavigationItem>
         <NavigationItem href="/">My Work</NavigationItem>
@@ -182,12 +181,12 @@ function RootLayoutInner({ children }) {
           layout
           id={panelId}
           style={{ height: expanded ? 'auto' : '0.5rem' }}
-          className="relative z-50 overflow-hidden bg-neutral-950 pt-2"
+          className="relative z-50 overflow-hidden bg-stone-200 pt-2"
           aria-hidden={expanded ? undefined : 'true'}
           inert={expanded ? undefined : ''}
         >
-          <motion.div layout className="bg-neutral-800">
-            <div ref={navRef} className="bg-neutral-950 pb-16 pt-14">
+          <motion.div layout className="bg-stone-200">
+            <div ref={navRef} className="bg-stone-200 pb-16 pt-14">
               <Header
                 invert
                 panelId={panelId}
