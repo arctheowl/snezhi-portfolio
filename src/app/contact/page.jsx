e2121565a89d58c77f79aae9d9875fa6bd9b0e59
SNVes <142303454+SNVes@users.noreply.github.com>
2023-08-18 17:03:41 +0100

    contact me page updates

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -42,69 +42,15 @@ function RadioInput({ label, ...props }) {
   )
 }
 
-function ContactForm() {
-  return (
-    <FadeIn className="lg:order-last">
-      <form>
-        <h2 className="font-display text-base font-semibold text-neutral-950">
-          Work inquiries
-        </h2>
-        <div className="isolate mt-6 -space-y-px rounded-2xl bg-white/50">
-          <TextInput label="Name" name="name" autoComplete="name" />
-          <TextInput
-            label="Email"
-            type="email"
-            name="email"
-            autoComplete="email"
-          />
-          <TextInput
-            label="Company"
-            name="company"
-            autoComplete="organization"
-          />
-          <TextInput label="Phone" type="tel" name="phone" autoComplete="tel" />
-          <TextInput label="Message" name="message" />
-          <div className="border border-neutral-300 px-6 py-8 first:rounded-t-2xl last:rounded-b-2xl">
-            <fieldset>
-              <legend className="text-base/6 text-neutral-500">Budget</legend>
-              <div className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2">
-                <RadioInput label="£25K - £50K" name="budget" value="25" />
-                <RadioInput label="£50K - £100K" name="budget" value="50" />
-                <RadioInput label="£100K - £150K" name="budget" value="100" />
-                <RadioInput label="More than £150K" name="budget" value="150" />
-              </div>
-            </fieldset>
-          </div>
-        </div>
-        <Button type="submit" className="mt-10">
-          Lets work together
-        </Button>
-      </form>
-    </FadeIn>
-  )
-}
 
 function ContactDetails() {
   return (
     <FadeIn>
-      <h2 className="font-display text-base font-semibold text-neutral-950">
-        Phone me
-      </h2>
-      <p className="mt-6 text-base text-neutral-600">
-        Prefer doing things in person? I dont but below is my phone number
-        incase you do
-      </p>
-      <p className="mt-4">12312341345</p>
-
       <Border className="mt-16 pt-16">
-        <h2 className="font-display text-base font-semibold text-neutral-950">
-          Email Me
-        </h2>
         <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
           {[
-            ['Personal', 'careers@studioagency.com'],
-            ['Work', 'careers@studioagency.com'],
-           
+            ['Email Me', 'snezhana.nikolaeva@gmail.com'],
+            ['LinkedIn', 'https://www.linkedin.com/in/snezhana-veskova/']           
           ].map(([label, email]) => (
             <div key={email}>
               <dt className="font-semibold text-neutral-950">{label}</dt>
@@ -132,16 +78,8 @@ export const metadata = {
 export default function Contact() {
   return (
     <>
-      <PageIntro eyebrow="Contact me" title="Let’s work together">
-        <p>I can’t wait to hear from you.</p>
+      <PageIntro eyebrow="Contact me" >
       </PageIntro>
-
-      <Container className="mt-24 sm:mt-32 lg:mt-40">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
-          <ContactForm />
-          <ContactDetails />
-        </div>
-      </Container>
     </>
   )
 }
